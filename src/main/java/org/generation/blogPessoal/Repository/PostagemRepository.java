4b8a4d043c74e082f4a3730fe1c2fbca52ecec36
Andressa Santos <andressasantosp0@gmail.com>
2021-03-19 15:03:42 -0300

    crud 19/03

--- src/main/java/org/generation/blogPessoal/Repository/PostagemRepository.java
+++ src/main/java/org/generation/blogPessoal/Repository/PostagemRepository.java
@@ -2,6 +2,7 @@ package org.generation.blogPessoal.Repository;
 
 import java.util.List;
 
+
 import org.generation.blogPessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
