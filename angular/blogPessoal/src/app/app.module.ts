a578038295193d39cd35913d76f093b5d80bac34
Andressa Santos <andressasantosp0@gmail.com>
2021-04-27 13:22:30 -0300

    homepage

--- angular/blogPessoal/src/app/app.module.ts
+++ angular/blogPessoal/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CadastroComponent } from './cadastro/cadastro.component';
     MenuComponent,
     FooterComponent,
     LoginComponent,
-    CadastroComponent
+    CadastroComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
