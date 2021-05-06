fe390ba5e141e6797b877a840efa0ad9c626782b
Andressa Santos <andressasantosp0@gmail.com>
2021-05-06 20:17:18 -0300

    métodos http

--- angular/blogPessoal/src/app/tema/tema.component.ts
+++ angular/blogPessoal/src/app/tema/tema.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
   templateUrl: './tema.component.html',
-  styleUrls: ['./tema.component.css']
+  styleUrls: ['./tema.component.css'],
 })
 export class TemaComponent implements OnInit {
+  tema: Tema = new Tema();
+  listaTemas: Tema[];
 
-  constructor() { }
+  constructor(private router: Router, private temaService: TemaService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    if (environment.token == '') {
+      this.router.navigate(['/login']);
+    }
   }
 
+  cadastrar() {
+    this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
+      this.tema = resp;
+      alert('Tema cadastrado com sucesso!');
+      this.tema = new Tema();
+    });
+  }
 }
